5c14ff3ad278618f3706a43993605e9ed18127a1
Wil <mreyeswilson@gmail.com>
2024-11-25 22:47:14 -0500

    adding gsheets integration

--- script.js
+++ script.js
@@ -23,6 +23,17 @@ const language = {
 
 };
 
+const url = "https://script.googleusercontent.com/macros/echo?user_content_key=YpLUABsxjjIoutlloQp-OEYdDINDfZG5ywOBNflEgNeBHf1QMM70i1i8Ai6YIFP7z9BQkV5SLEN6G2N2AoXr4MLuoYUhqa-fm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnMSk1hKidNPigzJD4VOywdWCAEAFGvgXH3IHta_Hv2_FLqzCk1VN72P5bIwK08l-mGOi-uRIrT1Rsa1D_gtQU_VKdYxk-bn_mNz9Jw9Md8uu&lib=MZj75W6tlZ-AOX3FuhBPRx0_xGF0snHn-"
+const getData = () => new Promise(async (resolve, reject) => {
+    const res = await fetch(url)
+    const data = await res.json()
+    resolve(data.map((item) => ({
+        fecha: dayjs(item.Fecha),
+        motivo: item.Motivo,
+        dow: item.DOW,
+    })))
+})
+
 const getFestivo = () => {
     const festivo = festivos.find(f => dayjs(f.fecha).isAfter(today, "day"));
     if (!festivo) return null;
@@ -38,34 +49,36 @@ const title = "Festivos Colombia " + new Date().getFullYear();
 
 document.addEventListener("DOMContentLoaded", update);
 
-
 function update() {
-    $("title").innerHTML = title;
-    $(".title").innerHTML = title;
-    $("#flipdown").innerHTML = ""
+    const loader = $("#loader")
+    getData().then((data) => {
+        $("title").innerHTML = title;
+        $(".title").innerHTML = title;
+        $("#flipdown").innerHTML = ""
+
+        
+        let {fecha, motivo} = data.find(f => f.fecha.isAfter(today, "day"));
     
-    let nextFextivo = getFestivo();
+        $(".next-festivo").innerHTML = `
+        <div class="text-left w-full">
+            <b class="block w-full text-black">Próximo festivo:</b> 
+            <span class="text-sm text-red-500">${fecha.format("dddd, DD [de] MMMM")} - ${motivo}</span>
+        <div>
+        `;
 
-    if (!nextFextivo) 
-        nextFextivo = {
-            fecha: 0,
-            nombre: ""
-        }
+        const milliseconds = fecha.valueOf() / 1000 ?? 0;
     
+        new FlipDown(milliseconds, {
+            language,
+            headings: ['Días', 'Horas', 'Minutos', 'Segundos'],
+        }).start().ifEnded(() => {
+           location.reload()
+        })
 
-    $(".next-festivo").innerHTML = `
-    <div class="text-left w-full">
-        <b class="block w-full text-black">Próximo festivo:</b> 
-        <span class="text-sm text-red-500">${dayjs(nextFextivo.fecha * 1000).format("dddd, DD [de] MMMM")} - ${nextFextivo?.nombre}</span>
-    <div>
-    `;
-
-    new FlipDown(nextFextivo.fecha, {
-        language,
-        headings: ['Días', 'Horas', 'Minutos', 'Segundos'],
-    }).start().ifEnded(() => {
-       location.reload()
+        loader.classList.add("hidden")
     })
+
+    
 }
 
 
